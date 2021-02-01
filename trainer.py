8f58eaf5390794ca201671cdda00408d76427a14
Yash Karbhari <60766134+yashkarbhari@users.noreply.github.com>
2021-02-01 11:05:32 +0530

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,1 +1,17 @@
+import os 
+import cv2
+import pickle
+import numpy as np
+import collections
+from collections import defaultdict
+import matplotlib.pyplot as plt
 
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Conv2D, LeakyReLU, Dropout, Flatten, MaxPooling2D
+from tensorflow.keras.layers import BatchNormalization, Embedding, Reshape, Activation
+from tensorflow.keras.layers import Concatenate, Conv2DTranspose, multiply, UpSampling2D
+from tensorflow.keras.initializers import RandomNormal
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import Progbar
