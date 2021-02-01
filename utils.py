06775bd7187fa855a1c2056f45b5dc88aa375d38
Yash Karbhari <60766134+yashkarbhari@users.noreply.github.com>
2021-02-01 18:10:28 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+import numpy as np
+import os
+import random
+
 def print_logs(metrics_names, train_history, test_history):
 
     print('{0:<22s} | {1:4s} | {2:15s} | {3:5s}'.format(
@@ -24,6 +28,7 @@ def generate_batch_noise_and_labels(batch_size, latent_dim):
     sampled_labels = np.random.randint(0, 2, batch_size)
 
     return noise, sampled_labels
+
 
 def label_smoothing(vector, max_dev = 0.2):
         d = max_dev * np.random.rand(vector.shape[0],vector.shape[1])
