2ac982135a00ab160bfe4623b9841a2a56fa4deb
Yash Karbhari <60766134+yashkarbhari@users.noreply.github.com>
2021-04-30 22:38:16 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,4 +10,23 @@ def label_smoothing(vector, max_dev = 0.2):
 valid_o = np.ones((bs, 1))
 fake_o = np.zeros((bs, 1))
 
+# Function for printing the logs after every epoch
+
+def print_logs(metrics_names, train_history, test_history):
+
+    print('{0:<22s} | {1:4s} | {2:15s} | {3:5s}'.format(
+        'component', *metrics_names))
+    print('-' * 65)
+
+    ROW_FMT = '{0:<22s} | {1:<4.2f} | {2:<15.2f} | {3:<5.2f}'
+    print(ROW_FMT.format('generator (train)',
+                         *train_history['generator'][-1]))
+    print(ROW_FMT.format('generator (test)',
+                         *test_history['generator'][-1]))
+    print(ROW_FMT.format('discriminator (train)',
+                         *train_history['discriminator'][-1]))
+    print(ROW_FMT.format('discriminator (test)',
+                         *test_history['discriminator'][-1]))
+
+
 
