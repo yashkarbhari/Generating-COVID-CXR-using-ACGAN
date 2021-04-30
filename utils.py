7e5322fc247a6d9ab4c845b1cf17d60fc4d3ca91
Yash Karbhari <60766134+yashkarbhari@users.noreply.github.com>
2021-04-30 22:36:57 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,13 @@
+# Function for label smoothing 
+
+def label_smoothing(vector, max_dev = 0.2):
+        d = max_dev * np.random.rand(vector.shape[0],vector.shape[1])
+        if vector[0][0] == 0:
+            return vector + d
+        else:
+            return vector - d
+          
+valid_o = np.ones((bs, 1))
+fake_o = np.zeros((bs, 1))
+
 
